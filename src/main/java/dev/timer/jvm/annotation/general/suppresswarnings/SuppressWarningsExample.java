2193f8e98e3712bd5c33e3f541fa9501c9a697e0
Park Jae Seong <timer973@naver.com>
2025-05-03 20:47:17 +0900

    [Annotation, Reflection] General Annotation : SuppressWarnings

---
+++ src/main/java/dev/timer/jvm/annotation/general/suppresswarnings/SuppressWarningsExample.java
@@ -0,0 +1,13 @@
+package dev.timer.jvm.annotation.general.suppresswarnings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"unchecked", "rawtypes", "unused"})
+public class SuppressWarningsExample {
+    public void something() {
+        List list = new ArrayList();
+        list.add("data");
+        System.out.println(list.get(0));
+    }
+}
