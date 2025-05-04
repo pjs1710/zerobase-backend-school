c1d7c19160470edd912df281119c6f14f1198bbc
Park Jae Seong <timer973@naver.com>
2025-05-04 20:49:19 +0900

    [Java Version 의 변화] Collection Factory

---
+++ src/test/java/dev/timer/javachange/java11/CollectionFactoryTest.java
@@ -0,0 +1,38 @@
+package dev.timer.javachange.java11;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CollectionFactoryTest {
+
+    @Test
+    void listFactory() {
+        // old - 불변
+        List<String> oldList = Collections.unmodifiableList(Arrays.asList("apple", "banana", "cherry"));
+
+        // new - 불변
+        List<String> newList = List.of("apple", "banana", "cherry");
+
+        assertThat(oldList.equals(newList));
+    }
+
+    @Test
+    void mapFactory() {
+        // old - 불변
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("key1", "value1");
+        dataMap.put("key2", "value2");
+        Map<String, String> oldMap = Collections.unmodifiableMap(dataMap);
+
+        // new - 불변
+        Map<String, String> newMap = Map.of(
+                "key1", "value1",
+                "key2", "value2"
+        );
+
+        assertThat(oldMap.equals(newMap));
+    }
+}
